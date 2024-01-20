c0e67cfd2c42f7b4314723354fb5c6bf450a16a0
mexanik619 <80208363+mexanik619@users.noreply.github.com>
2024-01-20 20:20:08 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookPro = () => {
+  return (
+    <div className="macbook-pro">
+      <div className="overlap-group-wrapper">
+        <div className="overlap-group">
+          <img className="rectangle" alt="Rectangle" src="rectangle-4.png" />
+          <img className="img" alt="Rectangle" src="rectangle-1.png" />
+          <div className="div" />
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+          <div className="rectangle-6" />
+          <div className="rectangle-7" />
+          <img className="vector" alt="Vector" src="vector.svg" />
+          <div className="text-wrapper">HOME</div>
+          <div className="text-wrapper-2">SHOP MEN</div>
+          <div className="text-wrapper-3">SHOP WOMEN</div>
+          <div className="text-wrapper-4">ABOUT US</div>
+          <div className="text-wrapper-5">CONTACT US</div>
+          <div className="text-wrapper-5">CONTACT US</div>
+          <div className="text-wrapper-6">LOG IN</div>
+          <div className="text-wrapper-7">envion</div>
+          <img
+            className="lets-go-for"
+            alt="Lets go for"
+            src="let-s-go-for-environment-friendly-and-recycled-cloth-brand-let-s-go-for-envion.png"
+          />
+          <img className="vector-2" alt="Vector" src="vector.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
